639264a8e6b88363a24154582c7f7addab443c3b
John Lunn <john@lunn-ltd.co.uk>
2021-01-24 12:04:35 +0000

    func

---
+++ function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def modify_string(original):
+    original += " that has been modified."
+    # only local function copy of variable has been modified
+
+def modify_string_return(original):
+    original += " that has been modified."
+    return original
+
+test_string = "This is a test string"
+
+modify_string(test_string)
+print(test_string)
+
+test_string = modify_string_return(test_string)
+print(test_string)
